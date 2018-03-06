7f749d2bdf073d732edeb644193b7d08c47107fe
hannd17 <29674287+hannd17@users.noreply.github.com>
2018-03-06 16:36:59 +0000

    test

--- demo.py
+++ demo.py
@@ -2,6 +2,11 @@ from blinkt import set_pixel, set_brightness, show, clear
 
 set_brightness(0.1)
 
-clear()
-set_pixel(0, 255, 255, 255)
-show()
+import time
+
+while True:
+    for i in range(8):
+        clear()
+        set_pixel(i, 255, 255, 0255
+        show()
+        time.sleep(0.05)
