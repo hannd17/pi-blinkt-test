8c505c467bc864d445430fa42622dd098e583eda
hannd17 <29674287+hannd17@users.noreply.github.com>
2018-03-06 17:06:51 +0000

    test

--- demo.py
+++ demo.py
@@ -1,6 +1,19 @@
-from blinkt import set_pixel, set_brightness, show, clear
+import colorsys
 import time
 
-clear()
-set_pixel(0, 255, 255, 255)
-show()
+from blinkt import set_brightness, set_pixel, show
+
+spacing = 360.0 / 16.0
+hue = 0
+
+set_brightness(0.1)
+
+while True:
+    hue = int(time.time() * 100) % 360
+    for x in range(8):
+        offset = x * spacing
+        h = ((hue + offset) % 360) / 360.0
+        r, g, b = [int(c * 255) for c in colorsys.hsv_to_rgb(h, 1.0, 1.0)]
+        set_pixel(x, r, g, b)
+    show()
+    time.sleep(0.001)
