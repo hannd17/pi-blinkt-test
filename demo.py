1884baec58b6a42da55e8ae6784b6013adcea591
hannd17 <29674287+hannd17@users.noreply.github.com>
2018-03-06 17:54:57 +0000

    brightness adjustments

--- demo.py
+++ demo.py
@@ -13,7 +13,7 @@ while True:
     for x in range(8):
         offset = x * spacing
         h = ((hue + offset) % 360) / 360.0
-        r, g, b = [int(c * 255) for c in colorsys.hsv_to_rgb(h, 1.0, 1.0)]
+        r, g, b = [int(c * 100) for c in colorsys.hsv_to_rgb(h, 1.0, 1.0)]
         set_pixel(x, r, g, b)
     show()
     time.sleep(0.001)
